4a38ec2f01b359588f08024700e90334f3e8189e
Jasim Uddin <jasimnabilfeed@gmail.com>
2026-03-15 00:47:41 +0600

    file based react router

---
+++ app/routes/category.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Category() {
+  return (
+    <div>Category Good Good Good Good Good Good Good Good Good Good Good Good Good Good</div>
+  )
+}
+
+export default Category
---
+++ react-router.config.js
@@ -0,0 +1,7 @@
+export default {
+  fsRoutes: {
+    extensions: ['ts', 'tsx', 'js', 'jsx'], // include jsx
+  },
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
+};
